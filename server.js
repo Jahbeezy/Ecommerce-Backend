a8fad1c5d8ab894eac76677b3bcec8e2322d0011
Justin Barner <justinrb14@gmail.com>
2022-02-13 02:05:57 -0800

    readme and video

--- server.js
+++ server.js
@@ -12,5 +12,6 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+  console.log(`App listening on port https://localhost:${PORT}`);
 });
+// https://watch.screencastify.com/v/qUvmtvx7pGpFCoShdUeP
